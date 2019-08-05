5d11fbfe72dbf680bc009e9cacf15053c2863ab7
emerihcelestin <emerih02@gmail.com>
2019-08-05 17:24:56 +0300

    gossip

---
+++ db/seeds.rb
@@ -0,0 +1,23 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+# 10.times do 
+# 	users = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Lorem.paragraph(sentence_count: 3, supplemental: false, random_sentences_to_add: 4), email: Faker::Internet.email, age: Faker::Number.between(from: 18, to: 90))
+# end
+# 10.times do 
+# 	cities = City.create(name: Faker::Nation.capital_city, zip_code: Faker::Address.zip_code)
+# end	
+# user = User.all 
+# my_city = []
+# user.each do |u|
+# 	rang = Faker::Number.between(from: 1, to: 10)
+# 	city = City.find(rang)
+# 	u.city = city
+# 	u.save
+# end
+
+ 

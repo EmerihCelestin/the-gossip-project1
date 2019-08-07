23291e2b772b2fc6a2fd09ce5b10f8280d5e27f5
emerihcelestin <emerih02@gmail.com>
2019-08-07 16:06:05 +0300

    rectification seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,38 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# 10.times do 
-# 	users = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Lorem.paragraph(sentence_count: 3, supplemental: false, random_sentences_to_add: 4), email: Faker::Internet.email, age: Faker::Number.between(from: 18, to: 90))
-# end
-# 10.times do 
-# 	cities = City.create(name: Faker::Nation.capital_city, zip_code: Faker::Address.zip_code)
-# end	
-# user = User.all 
-# my_city = []
-# user.each do |u|
-# 	rang = Faker::Number.between(from: 1, to: 10)
-# 	city = City.find(rang)
-# 	u.city = city
-# 	u.save
-# end
-# 20.times do 
-# 	gossips = Gossip.create(title: Faker::Music.album, content: Faker::Movie.quote)
-# end	
-# gossips = Gossip.all 
-# user = []
-# gossips.each do |g|
-# 	rang = Faker::Number.between(from: 1, to: 10)
-# 	user = User.find(rang)
-# 	g.user = user
-# 	g.save
-# end
-# 10.times do
-# 	tags = Tag.create(title: "##{Faker::Book.title}")
-# end 
-tags = Tag.all
-gossips= Gossip.all
+10.times do 
+	cities = City.create(name: Faker::Nation.capital_city, zip_code: Faker::Address.zip_code)
+end	
+
+10.times do 
+	users = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Lorem.paragraph(sentence_count: 3, supplemental: false, random_sentences_to_add: 4), email: Faker::Internet.email, age: Faker::Number.between(from: 18, to: 90),city_id: rang = Faker::Number.between(from: 1, to: 10))
+end
+
+20.times do 
+	gossips = Gossip.create(title: Faker::Music.album, content: Faker::Movie.quote,user_id: Faker::Number.between(from: 1, to: 10))
+end	
+
+10.times do
+	tags = Tag.create(title: "##{Faker::Book.title}")
+end 
+
 100.times do
-	a = rand(0..9)
-	b = rand(0..19)
-	tag_gossips = TagGossip.create(tag: tags[a], gossip: gossips[b])  
+	tag_gossips = TagGossip.create(tag_id: Faker::Number.between(from: 1, to: 10), gossip_id: Faker::Number.between(from: 1, to: 20))  
 end 
